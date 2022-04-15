754e3c933c41f4cd37df6ffe4449f73cc3d3355d
cjramosUCSD <103283539+cjramosUCSD@users.noreply.github.com>
2022-04-15 13:19:28 -0700

    now accounts if there is only the beginning bracket and not the last

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,23 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (toReturn.get(toReturn.size()-1).contains("[")){
+                toReturn.remove(toReturn.size()-1);
+            }
             currentIndex = closeParen + 1;
+
+
+
+
+            
+
         }
 
         return toReturn;

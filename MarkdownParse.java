ab0b2d217f14276094382c162eab3c3705c38e47
luisvelediaz <103283619+luisvelediaz@users.noreply.github.com>
2022-04-22 13:08:18 -0700

    When you have the test successfully passing, add and commit the changes, then push to Github. Provide the commit link below.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,53 +4,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
 public class MarkdownParse {
-
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            
-
             int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
+            int openParen = markdown.indexOf("(", openBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
-            if (markdown.contains("[") && markdown.contains("]")){
-                
+
+            if(markdown.substring(openBracket-1,openBracket).equals("!"))
+            {
+                currentIndex = closeParen + 1;
+            } 
+            else
+            {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-                if (toReturn.get(toReturn.size()-1).contains("[")){
-                    toReturn.remove(toReturn.size()-1);
-                }
-                
+                currentIndex = closeParen + 1;
             }
-
-            currentIndex = closeParen + 1;
-
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-
-
-            if (toReturn.get(toReturn.size()-1).contains("[")){
-                toReturn.remove(toReturn.size()-1);
-            }
-
-
         }
-        
-
-        
-    
 
         return toReturn;
-    }
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
     }
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,21 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+import java.nio.file.Files;
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-}
+    @Test
+    public void getLinksTest() throws IOException
+    {
+        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test-file.md");
+        String fileContent = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(fileContent);
+        assertEquals("The link should be https://something.com","https://something.com",links.get(0));
+        assertEquals("The links should be some-thing.html","some-thing.html",links.get(1));
+    }
+} 

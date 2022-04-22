81eb03a64e302ef9ac1461916f9b02ee5c73854d
luisvelediaz <103283619+luisvelediaz@users.noreply.github.com>
2022-04-22 13:42:33 -0700

    4-22 (1:40)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,27 +4,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 public class MarkdownParse {
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            int openQuotes = markdown.indexOf('"',currentIndex);
+            int closeQuotes = markdown.indexOf('"',openQuotes+1);
             int openBracket = markdown.indexOf("[", currentIndex);
-            int openParen = markdown.indexOf("(", openBracket);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int imageSyntax = markdown.indexOf("!",openBracket-1);
+            int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
+            boolean passes = false;
 
-            if(markdown.substring(openBracket-1,openBracket).equals("!"))
-            {
-                currentIndex = closeParen + 1;
-            } 
-            else
-            {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
+            while (!passes) {
+                if ((imageSyntax != -1) && imageSyntax == openBracket-1){ // skips if is image
+                    currentIndex = closeBracket+1;
+                    openBracket = markdown.indexOf("[", currentIndex);
+                } else if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
+                    // skips if enclosed by quotes
+                    currentIndex = closeQuotes+1;
+                } else {
+                    passes = true;
+                }
+                if (!passes) {
+                    closeBracket = markdown.indexOf("]", currentIndex);
+                    openParen = markdown.indexOf("(", closeBracket);
+                    closeParen = markdown.indexOf(")", openParen);
+                }
             }
+
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
 
         return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
     }
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,11 +4,14 @@ import java.nio.file.Path;
 import java.util.*;
 import java.nio.file.Files;
 import org.junit.*;
+
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
     @Test
     public void getLinksTest() throws IOException
     {
@@ -21,7 +24,7 @@ public class MarkdownParseTest {
 
     @Test
     public void getLinksForMyTestFile() throws IOException{
-        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test-file.md");
+        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = new MarkdownParse().getLinks(content);
 	    assertEquals(List.of("https://test.com","https://test.com"),links); 
@@ -29,7 +32,7 @@ public class MarkdownParseTest {
     @Test
     public void getImageLinkTest() throws IOException
     {
-        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test-file.md");
+        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test3.md");
         String fileContent = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(fileContent);
         assertEquals("There should be nothing in the arrayList of links",0,links.size());

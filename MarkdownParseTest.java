94c581d9ae1ae759c724e5fbe4e3c4196e6387b8
luisvelediaz <103283619+luisvelediaz@users.noreply.github.com>
2022-04-22 13:11:47 -0700

    Then, add separate test methods to test each of the test files you’ve written with getLinks. Make sure all the tests pass, and commit and push to Github. Provide the commit link below.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,4 +18,20 @@ public class MarkdownParseTest {
         assertEquals("The link should be https://something.com","https://something.com",links.get(0));
         assertEquals("The links should be some-thing.html","some-thing.html",links.get(1));
     }
+
+    @Test
+    public void getLinksForMyTestFile() throws IOException{
+        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new MarkdownParse().getLinks(content);
+	    assertEquals(List.of("https://test.com","https://test.com"),links); 
+    }
+    @Test
+    public void getImageLinkTest() throws IOException
+    {
+        Path fileName = Path.of("/Users/luisvelediaz/Documents/GitHub/markdown-parser2/test-file.md");
+        String fileContent = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(fileContent);
+        assertEquals("There should be nothing in the arrayList of links",0,links.size());
+    }
 } 

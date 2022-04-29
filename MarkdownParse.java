b949a5df2be5a3b8bc4e06e8712795ed49b4ccc3
luisvelediaz <103283619+luisvelediaz@users.noreply.github.com>
2022-04-29 12:26:23 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,6 @@
+
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,43 +13,44 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-<<<<<<< Updated upstream
             int openQuotes = markdown.indexOf('"',currentIndex);
             int closeQuotes = markdown.indexOf('"',openQuotes+1);
-=======
->>>>>>> Stashed changes
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int imageSyntax = markdown.indexOf("!",openBracket-1);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-<<<<<<< Updated upstream
-            
-            boolean passes = false;
 
-            while (!passes) {
-                if ((imageSyntax != -1) && imageSyntax == openBracket-1){ // skips if is image
-                    currentIndex = closeBracket+1;
-                    openBracket = markdown.indexOf("[", currentIndex);
-                } else if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
-                    // skips if enclosed by quotes
-                    currentIndex = closeQuotes+1;
-                } else {
-                    passes = true;
-                }
-                if (!passes) {
-                    closeBracket = markdown.indexOf("]", currentIndex);
+            while (openQuotes != -1 && closeQuotes != -1) { // skips brakets enclosed in quotes ""
+                if ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen)) {
+                    closeBracket = markdown.indexOf("]", closeBracket+1);
                     openParen = markdown.indexOf("(", closeBracket);
                     closeParen = markdown.indexOf(")", openParen);
+                } else {
+                    break;
                 }
             }
-
-=======
->>>>>>> Stashed changes
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            
+            if(openBracket == -1 || closeBracket == -1 || openParen == -1) { // avoid infinite loops
+                currentIndex = markdown.length();
+                break;
+            } else if ((imageSyntax != -1) && imageSyntax == openBracket-1){ // skips if is image
+                currentIndex = openBracket+1;
+            } else if (openParen != closeBracket+1) { // skips if parenthesis not right after close bracket
+                currentIndex = closeBracket+1;
+            } else if ((openQuotes != -1 && closeQuotes != -1) && ((openQuotes < openBracket && closeQuotes > closeBracket) || (openQuotes < openParen && closeQuotes > closeParen))) {
+                // skips if enclosed by quotes
+                currentIndex = closeQuotes+1;
+            } else {
+                if (openParen != -1 && closeParen != -1) {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                } else {
+                    break;
+                }
+            }
         }
-
+        
         return toReturn;
     }
 
@@ -58,3 +62,4 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
+
